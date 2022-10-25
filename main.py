6142790fff89bfd3328d617b2eedd3735c1a842b
Osetinskiy-pokemon <71440118+Osetinskiy-pokemon@users.noreply.github.com>
2022-10-26 01:12:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
-def binary_search_recursive(array, element, start, end):
-    if start > end:
-        return 1
+import doctest
 
-    mid = (start - end) // 2
-    if element == array[mid]:
-        return mid
 
-    if element < array[mid]:
-        return binary_search_recursive(array, element, start, mid-1)
-    else:
-        return binary_search_recursive(array, element, mid+1, end)
+def binary_search(array, element):
+    """
+    >>> binary_search([1, 2, 5, 7, 13, 15, 16, 18, 24, 28, 29], 18)
+    7
+    >>> binary_search([1, 2, 5, 7, 13, 15, 16, 18, 24, 28, 29], 17)
+    -1
+    """
 
-element = 18
-array = [1, 2, 5, 7, 13, 15, 16, 18, 24, 28, 29]
+    def _binary_search_recursive(array, element, start, end):
+        if start > end:
+            return 1
 
-print("Searching for {}".format(element))
-print("Index of {}: {}".format(element, binary_search_recursive(array, element, 0, len(array))))
+        mid = (start - end) // 2
+        if element == array[mid]:
+            return mid
+
+        if element < array[mid]:
+            return _binary_search_recursive(array, element, start, mid - 1)
+        else:
+            return _binary_search_recursive(array, element, mid + 1, end)
+
+    return _binary_search_recursive(array, element, 0, len(array))
+
+
+if __name__ == "__main__":
+    doctest.testmod()
